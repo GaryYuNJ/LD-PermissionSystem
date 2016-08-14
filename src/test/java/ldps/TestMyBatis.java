1ebc56b9df4cd9789b46bf9808aa1a494ea2af3c
GaryYu <Gary.Yu@bertelsmann.com.cn>
2016-08-14 14:12:10 +0800

    edit mybatis mapping path

--- src/test/java/ldps/TestMyBatis.java
+++ src/test/java/ldps/TestMyBatis.java
@@ -14,7 +14,7 @@ import com.ldps.service.IUserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类  
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})  
-  
+   
 public class TestMyBatis {  
     private static Logger logger = Logger.getLogger(TestMyBatis.class);  
     @Resource  
