c6e762326afdf8d6d19df41e9dfa570d6acd27f0
GaryYu <Gary.Yu@bertelsmann.com.cn>
2016-08-30 22:49:37 +0800

    resourcekey table define

---
+++ src/main/java/com/ldps/dao1/ResourceKeyMapper.java
@@ -0,0 +1,17 @@
+package com.ldps.dao1;
+
+import com.ldps.model.ResourceKey;
+
+public interface ResourceKeyMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ResourceKey record);
+
+    int insertSelective(ResourceKey record);
+
+    ResourceKey selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ResourceKey record);
+
+    int updateByPrimaryKey(ResourceKey record);
+}
